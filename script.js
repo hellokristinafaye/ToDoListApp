641aa2e1ddfad25e4d9672d7e8eef95cd21dbdb5
Kristina Faye <hellokristinafaye@gmail.com>
2025-03-22 17:15:08 -0700

    explainer comments added

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
+    // checks if there's anything in the input box. if not it sends and alert window up with the following message.
     if (inputBox.value === "") {
         alert("You must write something!");
     } else {
+        // creates an li element, and a JS access point to it
         let li = document.createElement('li');
+        // assigns the HTML content of that li element to whatever is entered in the input box
         li.innerHTML = inputBox.value;
+        // adds the li element to the listContainer element as a child
         listContainer.appendChild(li);
     }
 }
